cc5d294068835b9c0a312bec283f6514c960571c
Farjana Akter <farjanaakter178@gmail.com>
2024-09-02 09:32:24 +0600

    typescript class

---
+++ app.js
@@ -0,0 +1,11 @@
+// Learn tsc
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log("Test function");
+    };
+    return App;
+}());
+var a1 = new App();
+a1.test();
